8dcaa2c640c01b6d3ff8724e1e78216a163eb8b6
Fábio Henrique <psifabiohenrique@outlook.com>
2025-09-19 15:43:11 -0300

    Created models to patients and psy_records

--- src/patients/models.py
+++ src/patients/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Patient(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, 
+        on_delete=models.CASCADE,
+        related_name="patients"
+    )
+
+    first_name = models.CharField(max_length=100)
+    second_name = models.CharField(max_length=100, blank=True, null=True)
+    full_name = models.CharField(max_length=255, blank=True)
+    birth_date = models.DateField()
+
+    def save(self, *args, **kwargs):
+        if not self.full_name:
+            if self.second_name:
+                self.full_name = f'{self.first_name} {self.second_name}'
+            else:
+                self.full_name = self.first_name
+        super().save(*args, **kwargs)
+
+    @property
+    def records_count(self):
+        return self.psy_records.count()
+
+    def __str__(self):
+        return self.full_name
--- src/psy_records/models.py
+++ src/psy_records/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class PsyRecord(models.Model):
+    patient = models.ForeignKey(
+        'patients.Patient',
+        on_delete=models.CASCADE,
+        related_name="psy_records"
+    )
+    
+    record_number = models.PositiveIntegerField(editable=False)
+    date = models.DateField(default=timezone.now)
+    content = models.TextField()
+    
+    class Meta:
+        unique_together = ('patient', 'record_number')
+        ordering = ['patient', 'record_number']
+    
+    def save(self, *args, **kwargs):
+        if not self.record_number:
+            last_record = PsyRecord.objects.filter(patient=self.patient).order_by('-record_number').first()
+            if last_record:
+                self.record_number = last_record.record_number + 1
+            else:
+                self.record_number = 1
+        super().save(*args, **kwargs)
+    
+    def __str__(self):
+        return f"Prontuário #{self.record_number} - {self.patient.full_name}"
