afe402ce749a9baf618a20445d1e6857489c0a35
Fábio Henrique <psifabiohenrique@outlook.com>
2025-09-29 12:56:09 -0300

    Added clinical summarys to patient form. I.e., Objectives, demand, procedures, analysis and conclusions.

--- src/patients/models.py
+++ src/patients/models.py
@@ -8,10 +8,16 @@ class Patient(models.Model):
         related_name="patients"
     )
 
-    first_name = models.CharField(max_length=100)
-    second_name = models.CharField(max_length=100, blank=True, null=True)
-    full_name = models.CharField(max_length=255, blank=True)
-    birth_date = models.DateField()
+    first_name = models.CharField('Priemiro nome', max_length=100)
+    second_name = models.CharField('Segundo nome', max_length=100, blank=True, null=True)
+    full_name = models.CharField('Nome completo', max_length=255, blank=True)
+    birth_date = models.DateField('Data de nascimento')
+
+    objectives = models.TextField('Objetivos', blank=True, null=True)
+    clinical_demand = models.TextField('Demanda', blank=True, null=True)
+    clinical_procedures = models.TextField('Procedimentos', blank=True, null=True)
+    clinical_analysis = models.TextField('Análise', blank=True, null=True)
+    clinical_conclusion = models.TextField('Conclusões', blank=True, null=True)
 
     def save(self, *args, **kwargs):
         if not self.full_name:
